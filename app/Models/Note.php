75a5766703bcdd579e9710d83f2c4c1efaf398a3
Dev Parmar <devp2101@gmail.com>
2026-07-29 19:08:04 +0530

    - new migrations and models are created for the api.

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+#[Fillable([
+    'noteable_type',
+    'noteable_id',
+    'title',
+    'note',
+    'created_by',
+])]
 class Note extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * Get the parent noteable model.
+     */
+    public function noteable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * Get the user who created the note.
+     */
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
 }
