174dfaf191ba5779d960e0492c1fba117abcb768
Dev Parmar <devp2101@gmail.com>
2026-08-06 18:43:58 +0530

    - forgot-password apis are created.

--- app/Http/Requests/Auth/ForgotPasswordRequest.php
+++ app/Http/Requests/Auth/ForgotPasswordRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests\Auth;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ForgotPasswordRequest extends FormRequest
@@ -18,12 +17,23 @@ class ForgotPasswordRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, ValidationRule|array<mixed>|string>
+     * @return array[]
      */
     public function rules(): array
     {
         return [
             'email' => ['required', 'email'],
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function messages()
+    {
+        return [
+            'email.required' => 'Email is required',
+            'email.email' => 'Email is invalid',
         ];
     }
 }
